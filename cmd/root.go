7b661c0152bd02374f3b271a303cfb3b3692d794
Arseny Zarechnev <me@evindor.com>
2016-08-03 17:57:13 +0300

    copyrights

--- cmd/root.go
+++ cmd/root.go
@@ -1,4 +1,4 @@
-// Copyright © 2016 Arseny Zarechnev <arseny@sabaka.io>
+// Copyright © 2016 Sabaka OU <hello@sabaka.io>
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
